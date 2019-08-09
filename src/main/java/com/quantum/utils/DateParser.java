19daf403abba482714ee3dd48201d8d76d5a9d1c
Ilya Makhon <IlyaMakhon@coherentsolutions.com>
2019-08-09 10:25:52 +0300

    LocalDate changed to LocalDateTime

--- src/main/java/com/quantum/utils/DateParser.java
+++ src/main/java/com/quantum/utils/DateParser.java
@@ -2,17 +2,14 @@ package com.quantum.utils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.util.Date;
 
 public class DateParser {
 
     private static final String TIME_ZONE = "US/Central";
 
-    public LocalDate parseDateToUSZone(String dateStr) {
+    public LocalDateTime parseDateToUSZone(String dateStr) {
         Date date = null;
         try {
             date = new SimpleDateFormat("MM/dd/yyyy").parse(dateStr);
@@ -22,6 +19,6 @@ public class DateParser {
         ZonedDateTime utcZoned = ZonedDateTime.of(date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime(), ZoneOffset.UTC);
         ZoneId swissZone = ZoneId.of(TIME_ZONE);
         ZonedDateTime usZoned = utcZoned.withZoneSameInstant(swissZone);
-        return usZoned.toLocalDate().withDayOfMonth(usZoned.toLocalDate().getDayOfMonth() + 1);
+        return usZoned.toLocalDateTime().withDayOfMonth(usZoned.toLocalDateTime().getDayOfMonth() + 1);
     }
 }
