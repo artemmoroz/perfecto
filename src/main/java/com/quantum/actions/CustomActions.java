fbc1e70416d05200694732da7ef857f9f0b51967
Ilya Makhon <IlyaMakhon@coherentsolutions.com>
2019-08-16 18:21:36 +0300

    Rotation/SendFile/reportGeneration added

--- src/main/java/com/quantum/actions/CustomActions.java
+++ src/main/java/com/quantum/actions/CustomActions.java
@@ -4,9 +4,14 @@ import com.qmetry.qaf.automation.step.CommonStep;
 import com.qmetry.qaf.automation.ui.webdriver.QAFWebElement;
 import com.quantum.components.select.PerfectoSelect;
 import com.quantum.pages.MainPage;
+import com.quantum.utils.ConfigurationUtils;
 import com.quantum.utils.DeviceUtils;
+import com.quantum.utils.ReportUtils;
+
+import java.io.IOException;
 
 import static com.quantum.steps.PerfectoApplicationSteps.switchToContext;
+import static com.quantum.utils.PerfectoLabUtils.uploadMedia;
 
 /**
  * Created 31-Jul-19
@@ -25,6 +30,8 @@ public abstract class CustomActions {
     public abstract void waitForPresenceOfElement(QAFWebElement element);
 
     public abstract void clickOnText(String text);
+
+    public abstract PerfectoSelect getSelect(QAFWebElement element);
 
     public void startPerfectoApp() {
 //        try {
@@ -52,6 +59,35 @@ public abstract class CustomActions {
 
     }
 
-    public abstract PerfectoSelect getSelect(QAFWebElement element);
+    public void rotateDeviceLandscape() {
+        DeviceUtils.rotateDevice("landscape", "state");
+    }
+
+    public void rotateDevicePortait() {
+        DeviceUtils.rotateDevice("portrait", "state");
+    }
+
+
+
+    public void sendFile(String filePath, String fileName) {
+        try {
+            uploadMedia(
+                    "ps.perfectomobile.com",
+                    ConfigurationUtils.getBaseBundle().getPropertyValue("perfecto.capabilities.securityToken"),
+                    filePath,
+                    "PUBLIC:images/" + fileName
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void generateReport() {
+        try {
+            ReportUtils.generateTestReport(ConfigurationUtils.getBaseBundle().getPropertyValue("executionId"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
